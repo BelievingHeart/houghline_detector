278352817626676e90ce3f25f05cfe5644e81998
afterburner <rockey1024@icloud.com>
2018-12-06 14:35:24 +0800

    Finish if branches merging

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-#include <cmath>                        // for cos, sin
-#include <opencv2/core/cvdef.h>          // for CV_PI
-#include <iostream>                      // for operator<<, cout, ostream
-#include <opencv2/core/cvstd.hpp>        // for String
-#include <opencv2/core/fast_math.hpp>    // for cvRound
-#include <opencv2/core/mat.hpp>          // for Mat, MatExpr
-#include <opencv2/core/utility.hpp>      // for CommandLineParser
-#include "opencv2/highgui.hpp"           // for imshow, moveWindow, namedWindow
-#include "opencv2/imgproc.hpp"           // for cvtColor, line, putText, COL...
+#include "opencv2/highgui.hpp"        // for imshow, moveWindow, namedWindow
+#include "opencv2/imgproc.hpp"        // for cvtColor, line, putText, COL...
+#include <cmath>                      // for cos, sin
+#include <iostream>                   // for operator<<, cout, ostream
+#include <opencv2/core/cvdef.h>       // for CV_PI
+#include <opencv2/core/cvstd.hpp>     // for String
+#include <opencv2/core/fast_math.hpp> // for cvRound
+#include <opencv2/core/mat.hpp>       // for Mat, MatExpr
+#include <opencv2/core/utility.hpp>   // for CommandLineParser
 
 static cv::Mat color_image, output_image;
 int hough_type = 1;
@@ -72,84 +72,45 @@ int main(int argc, char **argv) {
   }
   cv::Canny(input_image, canny, 50, 200, 3);
 
-  if (hough_type == 1) {
+
     std::cout << "Increase decrease rho:        q, w\n"
                  "Increase decrease theta:      e, r\n"
                  "Increase decrease threshold:  t, y\n\n";
-    while (true) {
-      cv::cvtColor(blank, color_image, cv::COLOR_GRAY2BGR);
-      cv::cvtColor(input_image, output_image, cv::COLOR_GRAY2BGR);
-      std::vector<cv::Vec2f> lines;
-      cv::HoughLines(canny, lines, rho, theta, threshold, 0, 0);
-      for (const auto &line : lines) {
-        double rho_ = line[0], theta_ = line[1];
-        cv::Point pt1, pt2;
-        double a = cos(theta_), b = sin(theta_);
-        double x0 = a * rho_, y0 = b * rho_;
-        pt1.x = cvRound(x0 + 1000 * (-b));
-        pt1.y = cvRound(y0 + 1000 * (a));
-        pt2.x = cvRound(x0 - 1000 * (-b));
-        pt2.y = cvRound(y0 - 1000 * (a));
-        cv::line(output_image, pt1, pt2, cv::Scalar(0, 0, 255), 2, cv::LINE_AA);
-      }
-      cv::imshow(winName, output_image);
-      auto key = static_cast<char>(cv::waitKey(0));
-      if (key == 'q') {
-        increment(rho, 0.2, "rho");
-      }
-      if (key == 'w') {
-        if (rho > 0.2 + eps) {
-          increment(rho, -0.2, "rho");
-        }
-      }
-      if (key == 'e') {
-        increment(theta, CV_PI / 180, "theta");
-      }
-      if (key == 'r') {
-        if (theta > CV_PI / 180 + eps) {
-          increment(theta, -CV_PI / 180, "theta");
-        }
-      }
-      if (key == 't') {
-        if (threshold < 255 - eps) {
-          increment(threshold, 3, "treshold");
-        }
-      }
-      if (key == 'y') {
-        if (threshold > 3) {
-          increment(threshold, -3, "treshold");
-        }
-      }
-      if (static_cast<int>(key) == 27) {
-        break;
-      }
-      if (key == 'i') {
-        std::cout << std::string("current rho: \t\t") + std::to_string(rho) +
-                         "\n" + "current theta: \t\t" + std::to_string(theta) +
-                         "\n"
-                         "current threshold: \t" +
-                         std::to_string(threshold) + "\n";
-      }
-      cv::imshow(msgName, color_image);
+    if (hough_type != 1) {
+      std::cout << "Increase decrease minLineLen: a, s\n"
+                   "Increase decrease maxLineGap: d, f\n\n";
     }
-  } else {
 
-    std::cout << "Increase decrease rho:        q, w\n"
-                 "Increase decrease theta:      e, r\n"
-                 "Increase decrease threshold:  t, y\n"
-                 "Increase decrease minLineLen: a, s\n"
-                 "Increase decrease maxLineGap: d, f\n\n";
     while (true) {
       cv::cvtColor(blank, color_image, cv::COLOR_GRAY2BGR);
       cv::cvtColor(input_image, output_image, cv::COLOR_GRAY2BGR);
-      std::vector<cv::Vec4i> lines;
-      cv::HoughLinesP(canny, lines, rho, theta, threshold, minLineLen,
-                      maxLineGap);
-      for (const auto &line : lines) {
-        cv::line(output_image, cv::Point2i(line[0], line[1]),
-                 cv::Point2i(line[2], line[3]), cv::Scalar(0, 0, 255), 2,
-                 cv::LINE_AA);
+
+      if (hough_type == 1) {
+        std::vector<cv::Vec2f> lines;
+        cv::HoughLines(canny, lines, rho, theta, threshold, 0, 0);
+        for (const auto &line : lines) {
+          double rho_ = line[0], theta_ = line[1];
+          cv::Point pt1, pt2;
+          double a = cos(theta_), b = sin(theta_);
+          double x0 = a * rho_, y0 = b * rho_;
+          pt1.x = cvRound(x0 + 1000 * (-b));
+          pt1.y = cvRound(y0 + 1000 * (a));
+          pt2.x = cvRound(x0 - 1000 * (-b));
+          pt2.y = cvRound(y0 - 1000 * (a));
+          cv::line(output_image, pt1, pt2, cv::Scalar(0, 0, 255), 2,
+                   cv::LINE_AA);
+        }
+      } else {
+        std::vector<cv::Vec4i> lines;
+        cv::HoughLinesP(canny, lines, rho, theta, threshold, minLineLen,
+                        maxLineGap);
+        for (const auto &line : lines) {
+          cv::line(output_image, cv::Point2i(line[0], line[1]),
+                   cv::Point2i(line[2], line[3]), cv::Scalar(0, 0, 255), 2,
+                   cv::LINE_AA);
+        }
       }
+
       cv::imshow(winName, output_image);
       auto key = static_cast<char>(cv::waitKey(0));
       if (key == 'q') {
@@ -178,21 +139,24 @@ int main(int argc, char **argv) {
           increment(threshold, -3, "treshold");
         }
       }
-      if (key == 'a') {
 
-        increment(minLineLen, 3.0, "minLineLen");
-      }
-      if (key == 's') {
-        if (minLineLen > 3 + eps) {
-          increment(minLineLen, -3.0, "minLineLen");
+      if (hough_type != 1) {
+        if (key == 'a') {
+
+          increment(minLineLen, 3.0, "minLineLen");
         }
-      }
-      if (key == 'd') {
-        increment(maxLineGap, 3.0, "maxLineGap");
-      }
-      if (key == 'f') {
-        if (maxLineGap > 3 + eps) {
-          increment(maxLineGap, -3.0, "maxLineGap");
+        if (key == 's') {
+          if (minLineLen > 3 + eps) {
+            increment(minLineLen, -3.0, "minLineLen");
+          }
+        }
+        if (key == 'd') {
+          increment(maxLineGap, 3.0, "maxLineGap");
+        }
+        if (key == 'f') {
+          if (maxLineGap > 3 + eps) {
+            increment(maxLineGap, -3.0, "maxLineGap");
+          }
         }
       }
 
@@ -214,6 +178,6 @@ int main(int argc, char **argv) {
       }
       cv::imshow(msgName, color_image);
     }
-  }
+
   return 0;
 }
